d5bea74a1de077f443736910e19ae6adc9def561
alexm <alexander.martrain.2020@stpauls.com>
2025-01-29 18:36:15 -0600

    added average functionality and provided a letter grade based on a ten point grading scale; added

--- src/Grades.java
+++ src/Grades.java
@@ -6,41 +6,63 @@ import java.util.Scanner;
 public class Grades {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        double grade = 0;
+        double grade;
         double sum = 0;
         double count = 0;
-        while (grade >= 0) {
-            System.out.println("Please input a grade from 0 to 110 (-1 to terminate");
+        while (true) {
+            System.out.println("Please input a grade from 0 to 110 (-1 to terminate; 111 to calculate average of all inputted grades).");
             grade = input.nextDouble();
-            if (grade > 110) {
+
+            if (grade == -1) {
+                break;
+            } else if (grade == 111) {
+                if (count == 0) {
+                    System.out.println("No grades were entered.");
+                } else {
+                    double average = sum / count;
+                    System.out.printf("""
+                            Average Grade: %.2f
+                            Letter Grade: %s%n""", average, getLetterGrade(average));
+                    System.out.println("Would you like to enter more grades? Y/N");
+                    String continueInput = input.next();
+                    if (continueInput.equalsIgnoreCase("Y")) {
+                        System.out.println("Would you like to clear your previous grades? Y/N");
+                        String wipeGrades = input.next();
+                        if (wipeGrades.equalsIgnoreCase("Y")) {
+                            sum = 0;
+                            count = 0;
+                        }
+                        System.out.println("Please input a grade from 0 to 110 (-1 to terminate; 111 to calculate average of all inputted grades).");
+                        grade = input.nextDouble();
+                        sum += grade;
+                        count++;
+                    } else {
+                        break;
+                    }
+                }
+            } else if (grade < 0 || grade > 110) {
                 System.out.println("Invalid! Must be between 0 and 110.");
             } else {
                 sum += grade;
-                count++;
+                count ++;
             }
         }
+        input.close();
+    }
 
-//        Scanner scanner = new Scanner(System.in);
-//        String yesOrNo;
-//
-//        do {
-//            System.out.println("Enter your grade.");
-//            double grade = scanner.nextDouble();
-//            if (grade >= 90.0) {
-//                System.out.println("A");
-//            } else if (grade >= 80.0) {
-//                System.out.println("B");
-//            } else if (grade >= 70.0) {
-//                System.out.println("C");
-//            } else if (grade >= 60.0) {
-//                System.out.println("D");
-//            } else {
-//                System.out.println("F");
-//            }
-//            System.out.println("Would you like to get another grade? Y/N");
-//            yesOrNo = scanner.next();
-//        } while (yesOrNo.equalsIgnoreCase("Y"));
-//
-//        scanner.close();
+    public static String getLetterGrade(double average) {
+        if (average > 100.0) {
+            return "A+";
+        } else if (average >= 90.0) {
+            return "A";
+        } else if (average >= 80.0) {
+            return "B";
+        } else if (average >= 70.0) {
+            return "C";
+        } else if (average >= 60.0) {
+            return "D";
+        } else {
+            return "F";
+        }
     }
 }
