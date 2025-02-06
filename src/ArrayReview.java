02f666274ddb51518007972890b69eb6ad97ec00
alexm <alexander.martrain@selu.edu>
2025-02-06 11:41:16 -0600

    added ArrayReview

---
+++ src/ArrayReview.java
@@ -0,0 +1,33 @@
+package src;
+
+import java.util.Arrays;
+
+public class ArrayReview {
+    public static void main (String[] args) {
+        double[] doubleArray;
+        doubleArray = new double[10];
+//        System.out.println("Value of doubleArray[0] is " + doubleArray[0]);
+
+        for (int i = 0; i < doubleArray.length; i++) {
+            doubleArray[i] = i;
+        }
+        System.out.println("After");
+        printArray(doubleArray);
+        sumArray(doubleArray);
+    }
+
+    public static void printArray(double[] anyArray) {
+        // code in the function
+        for (int i = 0; i < anyArray.length; i++) {
+            System.out.println ("Value of doubleArray[" + i + "] is " + anyArray[i]);
+        }
+        System.out.println(Arrays.toString(anyArray));
+    }
+
+    public static void sumArray(double[] anyArray) {
+        for (int i = 0; i < anyArray.length; i++) {
+            int sum = 0;
+            sum += anyArray[i];
+        }
+    }
+}
