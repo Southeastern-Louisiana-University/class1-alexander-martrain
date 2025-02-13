7d78039f80b41b61d167a2f3cb645ec8e522404d
alexm <alexander.martrain.2020@stpauls.com>
2025-02-12 20:17:38 -0600

    added average functionality and provided a letter grade based on a ten point grading scale; added

--- src/Car.java
+++ src/Car.java
@@ -6,4 +6,55 @@ package src;
 
 public class Car {
 
+    private String make;
+    private String model;
+    private int year;
+
+    public Car(String make, String model, int year) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public void displayInfo() {
+        System.out.printf("displayInfo\nMake: %s\nModel: %s\nYear %d", make, model, year);
+    }
+
+    public static void main(String[] args) {
+        Car myCar = new Car("Toyota", "Corolla", 2022);
+        myCar.displayInfo();
+
+        // Modifying values using setter methods
+        myCar.setMake("Honda");
+        myCar.setModel("Civic");
+        myCar.setYear(2024);
+        System.out.printf("\ngetters\ngetMake: %s\ngetModel: %s\ngetYear: %d", myCar.getMake(), myCar.getModel(), myCar.getYear());
+
+        System.out.println("\nUpdated (via setters) Car Info:");
+        myCar.displayInfo();
+    }
 }
